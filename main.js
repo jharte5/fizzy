c2bde77ef23c00b37452a786bd5f891919fa8bbd
jharte5 <jonathan.harte@codeimmersives.com>
2019-10-01 15:22:21 -0400

    initial commit

--- main.js
+++ main.js
@@ -1,6 +1,36 @@
 function fizzy(num) {
+  let response = ' ';
+  if(num % 3 === 0 && num % 5 !== 0){
+
+  }
+  return response 
+}
+
+
+
+
+
+function fizzy(num) {
+let response = "FizzBuzz";
+if (num % 3 === 0 && num % 5 === 0){
+  response = "FizzBuzz"
+}
 
 }
+return response; 
+
+function fizzy(num) {
+  let response = ' ';
+  if(num % 3 !== 0 || num % 5 !== 0){
+    response = num
+
+  } else if()
+}
+
+
+
+
+
 
 module.exports = {
   fizzy,
